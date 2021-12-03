bc2df834ef8bb14cc84a54aae291b93c1c8aaac0
bendnorman <bdn29@cornell.edu>
2021-12-03 09:32:35 -0800

    Add option to write tables to csvs

--- src/dbcp/cli.py
+++ src/dbcp/cli.py
@@ -1,10 +1,32 @@
 """A Command line interface for the down ballot project."""
+import argparse
 import logging
 import sys
 
 import coloredlogs
 
 import dbcp
+
+
+def parse_command_line():
+    """
+    Parse script command line arguments. See the -h option.
+
+    Returns:
+        dict: A dictionary mapping command line arguments to their values.
+
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '-c',
+        '--csv',
+        action='store_true',
+        default=False,
+        help="Write tables to csvs. This will overwrite existing csvs.",
+    )
+    arguments = parser.parse_args()
+    return arguments
 
 
 def main():
@@ -14,7 +36,9 @@ def main():
     log_format = '%(asctime)s [%(levelname)8s] %(name)s:%(lineno)s %(message)s'
     coloredlogs.install(fmt=log_format, level='INFO', logger=dbcp_logger)
 
-    dbcp.etl.etl()
+    args = parse_command_line()
+
+    dbcp.etl.etl(args)
 
 
 if __name__ == "__main__":
