b901b586b78e9d9cdc1b93b909ca79a918556ca7
bendnorman <bdn29@cornell.edu>
2022-01-06 13:53:43 -0800

    Add big query and pandera support

--- src/dbcp/etl.py
+++ src/dbcp/etl.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from typing import Dict
 
 import pandas as pd
+import pandas_gbq
+import pydata_google_auth
 import sqlalchemy as sa
 
 import dbcp
@@ -53,9 +55,14 @@ def etl_pudl_tables() -> Dict[str, pd.DataFrame]:
 
 def etl(args):
     """Run dbc ETL."""
+    # Setup postgres
+    engine = dbcp.helpers.get_sql_engine()
+    with engine.connect() as con:
+        engine.execute("CREATE SCHEMA IF NOT EXISTS dbcp")
+
     etl_funcs = {
         "eipinfrastructure": etl_eipinfrastructure,
-        "pudl": etl_pudl_tables
+        # "pudl": etl_pudl_tables
     }
 
     # Extract and transform the data sets
@@ -65,11 +72,11 @@ def etl(args):
         transformed_dfs.update(etl_func())
 
     # Load table into postgres
-    engine = dbcp.helpers.get_sql_engine()
     with engine.connect() as con:
         for table_name, df in transformed_dfs.items():
             logger.info(f"Load {table_name} to postgres.")
-            df.to_sql(name=table_name, con=con, if_exists="replace", index=False)
+            df.to_sql(name=table_name, con=con, if_exists="replace",
+                      index=False, schema="dbcp")
 
     # TODO: Writing to CSVs is a temporary solution for getting data into Tableau
     # This should be removed once we have cloudsql setup.
@@ -78,5 +85,22 @@ def etl(args):
         output_path = Path("/app/output/")
         for table_name, df in transformed_dfs.items():
             df.to_csv(output_path / f"{table_name}.csv", index=False)
+
+    if args.upload_to_bigquery:
+        logger.info('Loading tables to BigQuery.')
+
+        SCOPES = [
+            'https://www.googleapis.com/auth/cloud-platform',
+            'https://www.googleapis.com/auth/drive',
+        ]
+
+        credentials = pydata_google_auth.get_user_credentials(
+            SCOPES)
+
+        for table_name, df in transformed_dfs.items():
+            logger.info(f"Loading: {table_name}")
+            pandas_gbq.to_gbq(
+                df, f"dbcp_data.{table_name}", project_id="dbcp-dev", if_exists="replace", credentials=credentials)
+            logger.info(f"Finished: {table_name}")
 
     logger.info("Sucessfully finished ETL.")
