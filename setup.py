4e07c635a582031b089903170b5dcdd0fecb650b
Austen Sharpe <austensharpe@gmail.com>
2022-01-05 17:29:44 -0700

    Add transformations to the ISO queue files that: - Cleans and standardizes resource - Classifies a resource as Renewable, Fossil, Hybrid, or Unknown Resource - Classifies a project as Renewable, Fossil, Hybrid, or Contains Unknown Resource
    Add a setup.py file
    
    Add a notebook that replicates the transform process.

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup script to make DBCP directly installable with pip."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='dbcp',
+    version='0.1.0',
+    url='https://github.com/catalyst-cooperative/down_ballot_climate.git',
+    author='Catalyst Cooperative',
+    author_email='hello@catalyst.coop',
+    description='Data engineering for the Down Ballot Climate Project (DBCP)',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=['catalystcoop.pudl>=0.5', 'psycopg2', 'tqdm', 'python-docx', ],
+)
