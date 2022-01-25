6ef69e97fb268c832f27fd2d8e13fd3fbe9f75f7
bendnorman <bdn29@cornell.edu>
2022-01-25 14:15:56 -0900

    Remove requirements.txt from conda env
    I think folks we getting confused by having a conda env and docker image
    with all of the requirements installed. All of the ETL processing should
    happen in a docker container, not the conda env. I removed requirements.txt
    from environment.yml. The conda env will only be used for running pre-commit hooks.
    The unit tests in the pre-commit hook are running inside a docker container
    instead of the conda environment.
    
    test_local_opposition.test_extract() broke so I made a fix in ColumbiaDocxParser().extract().

--- src/dbcp/extract/local_opposition.py
+++ src/dbcp/extract/local_opposition.py
@@ -1,14 +1,16 @@
-from typing import Dict, List, Optional
+"""Extraction logic for Columbia Local Opposition dataset."""
 from pathlib import Path
+from typing import Dict, List, Optional
 
-import pandas as pd
 import docx
+import pandas as pd
 
 from pudl.metadata.enums import US_STATES
 
 
 class ColumbiaDocxParser(object):
     """Parser for the Columbia Local Opposition .docx file."""
+
     POSSIBLE_STATES = set(US_STATES.values())
     POSSIBLE_HEADERS = {'Local Laws/Ordinances',
                         'Contested Projects', 'State Policy'}
@@ -18,6 +20,7 @@ class ColumbiaDocxParser(object):
     NULL_PROJECT = {'No contested projects were found at this time.', }
 
     def __init__(self) -> None:
+        """Create ColumbiaDocxParser object to parse docx data."""
         self.current_state = ''
         self.current_header = ''
         self.doc: Optional[docx.Document] = None
@@ -146,9 +149,10 @@ class ColumbiaDocxParser(object):
         # that produces an extra empty row
         query_str = "state == 'Texas' and (locality == 'Brownsville' or locality == 'Benbrook')"
         subset = output['local_ordinance'].query(query_str).reset_index()
-        if subset['ordinance'].iat[0] != "Enacted July 9, 2020:":
-            raise ValueError(
-                'Data has changed and Brownsville correction is no longer valid')
+        if not subset.empty:
+            if subset['ordinance'].iat[0] != "Enacted July 9, 2020:":
+                raise ValueError(
+                    'Data has changed and Brownsville correction is no longer valid')
         indices_to_delete = pd.Index(subset.groupby(
             ['state', 'locality'], as_index=False).first()['index'])
         output['local_ordinance'].drop(index=indices_to_delete, inplace=True)
