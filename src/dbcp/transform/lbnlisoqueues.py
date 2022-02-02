389c5a73aabf63fcb35f40a3c71912139a805513
Katie Lamb <katherine.lamb@catalyst.coop>
2022-02-02 09:53:18 -0800

    unrelated change to resource dict that austen wants merged

--- src/dbcp/transform/lbnlisoqueues.py
+++ src/dbcp/transform/lbnlisoqueues.py
@@ -56,7 +56,7 @@ RESOURCE_DICT = {
         "type": "Fossil"},
     "Nuclear": {
         "codes": ["NU", "NUC"],
-        "type": "Renewable"},
+        "type": "Unknown Resource"},
     "Offshore Wind": {
         "codes": [],
         "type": "Renewable"},
@@ -70,10 +70,10 @@ RESOURCE_DICT = {
         "codes": ["Wind", "WND", "Wind Turbine"],
         "type": "Renewable"},
     "Other": {
-        "codes": [], # 
+        "codes": [],
         "type": "Unknown Resource"},
     "Unknown": {
-        "codes": ["Wo", "F", "Hybrid", "M",],
+        "codes": ["Wo", "F", "Hybrid", "M"],
         "type": "Unknown Resource"},
     "Other Storage": {
         "codes": ["Flywheel", "Storage"],
