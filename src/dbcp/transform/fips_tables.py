755a18462ae185652cd536cb93e7922838a84634
t-desktop <tpbush@gmail.com>
2022-03-10 10:57:50 -0800

    add county and state fips table ETL

---
+++ src/dbcp/transform/fips_tables.py
@@ -0,0 +1,87 @@
+from typing import Dict, Sequence
+import logging
+import pandas as pd
+
+from dbcp.schemas import TABLE_SCHEMAS
+
+logger = logging.getLogger(__name__)
+
+def county_fips(counties: pd.DataFrame) -> pd.DataFrame:
+    """
+    Apply transformations to county FIPS table.
+
+    Args:
+        counties: raw county_fips table.
+
+    Returns:
+        transformed county_fips table.
+    """
+    counties = counties.copy()
+    counties = _dedupe_keep_shortest_name(counties, idx_cols=['statefp', 'countyfp'])
+    
+    # make 5 digit FIPS
+    counties['county_id_fips'] = counties['statefp'] + counties['countyfp']
+
+    rename_dict = {'statefp': 'state_id_fips', 'name': 'county_name',}
+    counties = (counties.rename(columns=rename_dict).drop(columns='countyfp'))
+
+    # Validate schema
+    counties = TABLE_SCHEMAS["county_fips"].validate(counties)
+    assert "object" not in counties.dtypes
+
+    return counties
+
+def state_fips(states: pd.DataFrame) -> pd.DataFrame:
+    """
+    Apply transformations to county FIPS table.
+
+    Args:
+        states: raw county_fips table.
+
+    Returns:
+        transformed county_fips table.
+    """
+    states = states.copy()
+    states = _dedupe_keep_shortest_name(states, idx_cols=['fips',])
+    
+    rename_dict = {'fips': 'state_id_fips', 'name': 'state_name', 'postal': 'state_abbrev'}
+    states = states.rename(columns=rename_dict)
+
+    # Validate schema
+    states = TABLE_SCHEMAS["state_fips"].validate(states)
+    assert "object" not in states.dtypes
+
+    return states
+
+
+def transform(fips_tables: Dict[str, pd.DataFrame]) -> pd.DataFrame:
+    """
+    Transform state and county FIPS dataframes.
+
+    Args:
+        fips_tables: Dictionary of the raw extracted data for each FIPS table.
+
+    Returns:
+        transformed_fips_tables: Dictionary of the transformed tables.
+    """
+    transformed_fips_tables = {}
+
+    transform_functions = {
+        "county_fips": county_fips,
+        "state_fips": state_fips,
+    }
+
+    for table_name, transform_func in transform_functions.items():
+        logger.info(f"FIPS tables: Transforming {table_name} table.")
+
+        table_df = fips_tables[table_name].copy()
+        transformed_fips_tables[table_name] = transform_func(table_df)
+
+    return transformed_fips_tables
+
+def _dedupe_keep_shortest_name(df: pd.DataFrame, idx_cols: Sequence[str], name_col: str='name') -> pd.DataFrame:
+    
+    sorted_idx = df[name_col].str.len().sort_values(ascending=True).index
+    sorted_ = df.loc[sorted_idx,:]
+    deduped = sorted_.drop_duplicates(subset=idx_cols, keep='first').sort_values(idx_cols)
+    return deduped
