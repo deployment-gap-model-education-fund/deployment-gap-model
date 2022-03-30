92e9fe020dc967b01488eb8f687c53d0aec41450
t-desktop <tpbush@gmail.com>
2022-03-29 23:03:52 -0700

    add dbcp.datamart.counties to top level __init__.py

--- src/dbcp/__init__.py
+++ src/dbcp/__init__.py
@@ -2,6 +2,7 @@
 import dbcp.etl  # noqa: F401
 import dbcp.data_mart  # noqa: F401
 import dbcp.data_mart.projects  # noqa: F401
+import dbcp.data_mart.counties  # noqa: F401
 import dbcp.extract  # noqa: F401
 import dbcp.extract.eipinfrastructure  # noqa: F401
 import dbcp.extract.lbnlisoqueues  # noqa: F401
