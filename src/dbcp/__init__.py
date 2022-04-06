9fc0a84507b1d7b8488a517897d53390572d33ea
t-desktop <tpbush@gmail.com>
2022-04-05 18:23:07 -0700

    draft proposed vs existing dashboard data mart tables

--- src/dbcp/__init__.py
+++ src/dbcp/__init__.py
@@ -3,6 +3,7 @@ import dbcp.etl  # noqa: F401
 import dbcp.data_mart  # noqa: F401
 import dbcp.data_mart.projects  # noqa: F401
 import dbcp.data_mart.counties  # noqa: F401
+import dbcp.data_mart.proposed_power_dash  # noqa: F401
 import dbcp.extract  # noqa: F401
 import dbcp.extract.eipinfrastructure  # noqa: F401
 import dbcp.extract.lbnlisoqueues  # noqa: F401
