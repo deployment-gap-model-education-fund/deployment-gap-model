0d31ec5e11c6ac540a094a163a9f8347bd6e3e86
t-desktop <tpbush@gmail.com>
2022-04-15 17:39:57 -0700

    apply black and pre-commit

--- src/dbcp/data_mart/proposed_power_dash.py
+++ src/dbcp/data_mart/proposed_power_dash.py
@@ -1,13 +1,20 @@
 """Module to create a county-level table of local ordinances for use in Tableau map layers.
 
-The motivating dashboard for this table is the proposed vs existing power dashboard."""
+The motivating dashboard for this table is the proposed vs existing power dashboard.
+"""
 
-from typing import Optional, Dict
+from typing import Dict, Optional
 
-import sqlalchemy as sa
 import pandas as pd
+import sqlalchemy as sa
+
+from dbcp.data_mart.helpers import (
+    CountyOpposition,
+    _get_county_fips_df,
+    _get_state_fips_df,
+    _subset_db_columns,
+)
 from dbcp.helpers import get_sql_engine
-from dbcp.data_mart.helpers import CountyOpposition, _get_county_fips_df, _get_state_fips_df, _subset_db_columns
 
 
 def local_opposition(
@@ -32,19 +39,22 @@ def local_opposition(
     if state_fips_df is None:
         state_fips_df = _get_state_fips_df(engine)
 
-    aggregator = CountyOpposition(engine=engine, county_fips_df=county_fips_df, state_fips_df=state_fips_df)
+    aggregator = CountyOpposition(
+        engine=engine, county_fips_df=county_fips_df, state_fips_df=state_fips_df
+    )
     county_opp = aggregator.agg_to_counties(include_state_policies=True)
 
     # bring in county names and state_id_fips
-    county_opp = county_opp.merge(county_fips_df, on='county_id_fips', copy=False)
-    county_opp = county_opp.merge(state_fips_df, on='state_id_fips',
-                                  copy=False)  # bring in state name and abbreviation
+    county_opp = county_opp.merge(county_fips_df, on="county_id_fips", copy=False)
+    county_opp = county_opp.merge(
+        state_fips_df, on="state_id_fips", copy=False
+    )  # bring in state name and abbreviation
     rename_dict = {
-        'geocoded_locality_name': 'ordinance_jurisdiction_name',
-        'geocoded_locality_type': 'ordinance_jurisdiction_type',
-        'earliest_year_mentioned': 'ordinance_earliest_year_mentioned',
-        'state_name': 'state',
-        'county_name': 'county',
+        "geocoded_locality_name": "ordinance_jurisdiction_name",
+        "geocoded_locality_type": "ordinance_jurisdiction_type",
+        "earliest_year_mentioned": "ordinance_earliest_year_mentioned",
+        "state_name": "state",
+        "county_name": "county",
     }
     county_opp.rename(columns=rename_dict, inplace=True)
     return county_opp
@@ -117,18 +127,18 @@ def _get_existing_plants(engine: sa.engine.Engine) -> pd.DataFrame:
     df = pd.read_sql(query, engine)
     # harmonize categories with ISO queue
     resource_map = {
-        'gas': 'Natural Gas',
-        'wind': 'Onshore Wind',
-        'hydro': 'Hydro',
-        'oil': 'Oil',
-        'nuclear': 'Nuclear',
-        'coal': 'Coal',
-        'other': 'Other',
-        'solar': 'Solar',
-        'Battery Storage': 'Battery Storage',
-        'Offshore Wind': 'Offshore Wind',
+        "gas": "Natural Gas",
+        "wind": "Onshore Wind",
+        "hydro": "Hydro",
+        "oil": "Oil",
+        "nuclear": "Nuclear",
+        "coal": "Coal",
+        "other": "Other",
+        "solar": "Solar",
+        "Battery Storage": "Battery Storage",
+        "Offshore Wind": "Offshore Wind",
     }
-    df.loc[:, 'resource'] = df.loc[:, 'resource'].map(resource_map)
+    df.loc[:, "resource"] = df.loc[:, "resource"].map(resource_map)
     return df
 
 
@@ -216,7 +226,9 @@ def _get_proposed_plants(engine: sa.engine.Engine) -> pd.DataFrame:
     return df
 
 
-def _add_has_ordinance_column(*, county_df: pd.DataFrame, local_opp: pd.DataFrame) -> pd.DataFrame:
+def _add_has_ordinance_column(
+    *, county_df: pd.DataFrame, local_opp: pd.DataFrame
+) -> pd.DataFrame:
     """Add boolean column has_ordinance to indicate presence of local opposition.
 
     Args:
@@ -226,13 +238,20 @@ def _add_has_ordinance_column(*, county_df: pd.DataFrame, local_opp: pd.DataFram
     Returns:
         pd.DataFrame: county_df plus a new column
     """
-    out = county_df.merge(local_opp[['has_ordinance', 'county_id_fips']], how='left',
-                          on='county_id_fips', copy=False, validate='m:1')
-    out.loc[:, 'has_ordinance'] = out.loc[:, 'has_ordinance'].fillna(False)
+    out = county_df.merge(
+        local_opp[["has_ordinance", "county_id_fips"]],
+        how="left",
+        on="county_id_fips",
+        copy=False,
+        validate="m:1",
+    )
+    out.loc[:, "has_ordinance"] = out.loc[:, "has_ordinance"].fillna(False)
     return out
 
 
-def _add_permitting_type_column(local_opp: pd.DataFrame, engine: sa.engine.Engine) -> pd.DataFrame:
+def _add_permitting_type_column(
+    local_opp: pd.DataFrame, engine: sa.engine.Engine
+) -> pd.DataFrame:
     """Add column permitting_type to indicate state/local/hybrid wind permitting.
 
     Args:
@@ -242,12 +261,18 @@ def _add_permitting_type_column(local_opp: pd.DataFrame, engine: sa.engine.Engin
     Returns:
         pd.DataFrame: local_opp plus a new column
     """
-    permit_df = _subset_db_columns(['state_id_fips', 'permitting_type'], 'dbcp.ncsl_state_permitting', engine)
-    out = local_opp.merge(permit_df, how='left', on='state_id_fips', copy=False, validate='m:1')
+    permit_df = _subset_db_columns(
+        ["state_id_fips", "permitting_type"], "dbcp.ncsl_state_permitting", engine
+    )
+    out = local_opp.merge(
+        permit_df, how="left", on="state_id_fips", copy=False, validate="m:1"
+    )
     return out
 
 
-def make_dashboard_tables(engine: Optional[sa.engine.Engine] = None) -> Dict[str, pd.DataFrame]:
+def make_dashboard_tables(
+    engine: Optional[sa.engine.Engine] = None,
+) -> Dict[str, pd.DataFrame]:
     """API function to create the 3 tables used in the existing vs proposed power dashboard.
 
     Args:
@@ -259,12 +284,14 @@ def make_dashboard_tables(engine: Optional[sa.engine.Engine] = None) -> Dict[str
     if engine is None:
         engine = get_sql_engine()
     dfs = {
-        'local_opp': local_opposition(engine=engine),
-        'existing_plants': _get_existing_plants(engine),
-        'proposed_plants': _get_proposed_plants(engine),
+        "local_opp": local_opposition(engine=engine),
+        "existing_plants": _get_existing_plants(engine),
+        "proposed_plants": _get_proposed_plants(engine),
     }
-    for key in ['existing_plants', 'proposed_plants']:
-        dfs[key] = _add_has_ordinance_column(county_df=dfs[key], local_opp=dfs['local_opp'])
-    dfs['local_opp'] = _add_permitting_type_column(dfs['local_opp'], engine=engine)
+    for key in ["existing_plants", "proposed_plants"]:
+        dfs[key] = _add_has_ordinance_column(
+            county_df=dfs[key], local_opp=dfs["local_opp"]
+        )
+    dfs["local_opp"] = _add_permitting_type_column(dfs["local_opp"], engine=engine)
 
     return dfs
