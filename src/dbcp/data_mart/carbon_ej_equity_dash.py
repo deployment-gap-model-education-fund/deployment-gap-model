dd9a33538c3bd1f10aca7f2e36079a5d110cf353
t-desktop <tpbush@gmail.com>
2022-04-21 23:10:50 -0700

    WIP convert existing plant data source to EIA923

--- src/dbcp/data_mart/carbon_ej_equity_dash.py
+++ src/dbcp/data_mart/carbon_ej_equity_dash.py
@@ -8,141 +8,208 @@ from typing import Optional
 import pandas as pd
 import sqlalchemy as sa
 
-from dbcp.helpers import get_sql_engine
+from dbcp.data_mart.helpers import _get_county_fips_df, _get_state_fips_df
+from dbcp.helpers import download_pudl_data, get_sql_engine
+from dbcp.transform.helpers import add_county_fips_with_backup_geocoding
 
 
-def _get_existing_fossil_plants(engine: sa.engine.Engine) -> pd.DataFrame:
-    # see last SELECT statement for output columns
-    query = """
-    WITH
-    projects as (
-        SELECT
-            county_id_fips,
-            state_id_fips,
-            plant_id_eia,
-            generator_id,
-            prime_mover_code,
-            fuel_type_code_pudl,
-            total_mmbtu,
-            net_generation_mwh,
-            capacity_mw,
-            latitude,
-            longitude
-        FROM data_warehouse.mcoe
-        WHERE operational_status = 'existing'
-        AND fuel_type_code_pudl in ('gas', 'oil', 'coal')
-    ),
-    w_county_names as (
-    select
-        cfip.county_name as county,
-        proj.*
-    from projects as proj
-    left join data_warehouse.county_fips as cfip
-        on proj.county_id_fips = cfip.county_id_fips
-    ),
-    w_names as (
-        SELECT
-            sfip.state_name as state,
-            proj.*
-        from w_county_names as proj
-        left join data_warehouse.state_fips as sfip
-            on proj.state_id_fips = sfip.state_id_fips
+def _get_existing_plant_fuel_data() -> pd.DataFrame:
+    pudl_data_path = download_pudl_data()
+    pudl_engine = sa.create_engine(
+        f"sqlite:////{pudl_data_path}/pudl_data/sqlite/pudl.sqlite"
     )
-    select
-        proj.*,
-        ncsl.permitting_type
-    from w_names as proj
-    left join data_warehouse.ncsl_state_permitting as ncsl
-        on proj.state_id_fips = ncsl.state_id_fips
-    ;
 
+    query = """
+    select
+        plant_id_eia,
+        fuel_type_code_pudl,
+        prime_mover_code,
+        report_date,
+        net_generation_mwh as mwh,
+        fuel_consumed_for_electricity_mmbtu as mmbtu
+    from generation_fuel_eia923
+    where report_date >= date('2020-01-01') -- this is monthly data
+    AND fuel_type_code_pudl in ('coal', 'gas', 'oil')
+    ;
     """
-    df = pd.read_sql(query, engine)
+    df = pd.read_sql(query, pudl_engine)
     return df
 
 
-def _add_emissions_factors(df: pd.DataFrame) -> None:
+def _combine_cc_parts(gen_fuel_923: pd.DataFrame) -> pd.DataFrame:
+    # split, apply, combine
+    # split
+    is_cc = gen_fuel_923.loc[:, "prime_mover_code"].isin(
+        {
+            "CA",
+            "CT",
+        }
+    )
+    cc = gen_fuel_923.loc[is_cc, :]
+    # apply
+    cc = cc.groupby(
+        ["plant_id_eia", "report_date", "fuel_type_code_pudl"], as_index=False
+    )[["mwh", "mmbtu"]].sum()
+    cc["prime_mover_code"] = "CC"
+    # combine
+    non_cc = gen_fuel_923.loc[~is_cc, :]
+    out = pd.concat([cc, non_cc], axis=0, ignore_index=True)
+    return out
+
+
+def _add_emissions_factors(gen_fuel_923: pd.DataFrame) -> None:
     # https://www.ecfr.gov/current/title-40/chapter-I/subchapter-C/part-98/subpart-C/appendix-Table%20C-1%20to%20Subpart%20C%20of%20Part%2098
-    epa_emissions_factors = {  # from EPA 40 CFR 98. kg CO2e/mmbtu
-        "gas": 53.06,
-        "coal": 95.52,  # power sector average
-        "oil": 73.96,  # DFO #2
+    # from EPA 40 CFR 98. I divide by 1000 to convert kg_CO2e/mmbtu to metric tonnes
+    epa_emissions_factors = {
+        "gas": 53.06 / 1000,
+        "coal": 95.52 / 1000,  # power sector average
+        "oil": 73.96 / 1000,  # DFO #2
     }
-    df["kg_co2_per_mmbtu"] = df.loc[:, "fuel_type_code_pudl"].map(epa_emissions_factors)
+    gen_fuel_923["tonnes_co2_per_mmbtu"] = gen_fuel_923.loc[
+        :, "fuel_type_code_pudl"
+    ].map(epa_emissions_factors)
 
 
-def _estimate_co2e_from_fuel_burn(df: pd.DataFrame) -> None:
-    """Estimate CO2e from fuel reporting. IT IS MISSING ALL GAS TURBINES.
-
-    This accounts for 88% of net generation, but only 27% of generators.
-    """
-    _add_emissions_factors(df)
-    kg_per_tonne = 1000
-    df["co2e_tonnes_per_year"] = (
-        df.loc[:, "total_mmbtu"] * df.loc[:, "kg_co2_per_mmbtu"] / kg_per_tonne
+def _co2_from_mmbtu(
+    gen_fuel_923: pd.DataFrame,
+    output_name: str = "co2e_tonnes_via_mmbtu",
+    mmbtu_col: str = "mmbtu",
+) -> None:
+    """Estimate CO2e from fuel reporting."""
+    if "tonnes_co2_per_mmbtu" not in gen_fuel_923.columns:
+        _add_emissions_factors(gen_fuel_923)
+    gen_fuel_923[output_name] = (
+        gen_fuel_923.loc[:, mmbtu_col] * gen_fuel_923.loc[:, "tonnes_co2_per_mmbtu"]
     )
     return
 
 
-def _estimate_co2e_from_net_generation(df: pd.DataFrame) -> None:
-    # https://www.eia.gov/electricity/annual/html/epa_08_02.html
-    heat_rates = {  # noqa: F841
-        "coal_st": 9997.0,
-        "gas_st": 10368.0,
-        "gas_gt": 11069.0,
-        "gas_cc": 7604.0,
-        "oil_ic": 10334.0,
-    }
-    raise NotImplementedError
-
-
-def _harmonize_fuels_with_iso_queue(
-    df: pd.DataFrame, fuel_type_col: str = "resource"
+def _co2_from_mwh(
+    df: pd.DataFrame,
+    output_name: str = "co2e_tonnes_via_mwh",
+    mwh_col: str = "mwh",
 ) -> None:
-    resource_map = {
-        "gas": "Natural Gas",
-        "wind": "Onshore Wind",
-        "hydro": "Hydro",
-        "oil": "Oil",
-        "nuclear": "Nuclear",
-        "coal": "Coal",
-        "other": "Other",
-        "solar": "Solar",
-        "Battery Storage": "Battery Storage",
-        "Offshore Wind": "Offshore Wind",
+    # https://www.eia.gov/electricity/annual/html/epa_08_02.html
+    heat_rates_mmbtu_per_mwh = {
+        "coal_CC": 9.997,  # not a standard prime mover code!
+        "coal_GT": 9.997,
+        "coal_IC": 9.997,
+        "coal_OT": 9.997,
+        "coal_ST": 9.997,
+        "gas_ST": 10.368,
+        "gas_OT": 10.368,
+        "gas_IC": 8.832,
+        "gas_GT": 11.069,
+        "gas_FC": 7.604,
+        "gas_CC": 7.604,  # not a standard prime mover code!
+        "gas_CS": 7.604,
+        "gas_CE": 7.604,
+        "oil_CC": 9.208,  # not a standard prime mover code!
+        "oil_CS": 9.208,
+        "oil_GT": 13.223,
+        "oil_IC": 10.334,
+        "oil_ST": 10.339,
     }
-    df.loc[:, fuel_type_col] = df.loc[:, fuel_type_col].map(resource_map)
+    if "tonnes_co2_per_mmbtu" not in df.columns:
+        _add_emissions_factors(df)
+    df["fuel_prime_mover"] = (
+        df.loc[:, "fuel_type_code_pudl"] + "_" + df.loc[:, "prime_mover_code"]
+    )
+    df["mmbtu_per_mwh"] = df.loc[:, "fuel_prime_mover"].map(heat_rates_mmbtu_per_mwh)
+
+    df[output_name] = (
+        df.loc[:, mwh_col]
+        * df.loc[:, "mmbtu_per_mwh"]
+        * df.loc[:, "tonnes_co2_per_mmbtu"]
+    )
+    intermediates = ["tonnes_co2_per_mmbtu", "mmbtu_per_mwh", "fuel_prime_mover"]
+    df.drop(columns=intermediates, inplace=True)
+    return
 
 
-def _dummy_existing_co2_table(engine: sa.engine.Engine) -> pd.DataFrame:
-    df = _get_existing_fossil_plants(engine=engine)
-    _estimate_co2e_from_fuel_burn(df)
-    groups = df.groupby("plant_id_eia")
-    categorical_cols = [
-        "state",
-        "county",
-        "county_id_fips",
-        "state_id_fips",
-        "fuel_type_code_pudl",
-        "latitude",
-        "longitude",
-        "permitting_type",
-    ]
-    quantitative_cols = [
-        "total_mmbtu",
-        "net_generation_mwh",
-        "capacity_mw",
-        "co2e_tonnes_per_year",
-    ]
-    plant_data = groups[categorical_cols].nth(0)
-    quant_data = groups[quantitative_cols].sum()
-    plant_data = plant_data.join(quant_data).reset_index()
+def _estimate_existing_co2e(gen_fuel_923: pd.DataFrame) -> pd.DataFrame:
+    co2e = _combine_cc_parts(gen_fuel_923)
+    _co2_from_mmbtu(co2e)
+    _co2_from_mwh(co2e)
+    # use mmbtu calc unless it is zero, in which case use the max estimate.
+    condition = co2e.loc[:, "co2e_tonnes_via_mmbtu"].gt(0)
+    alternative = co2e.loc[:, ["co2e_tonnes_via_mmbtu", "co2e_tonnes_via_mwh"]].max(
+        axis=1
+    )
+    co2e["co2e_tonnes_per_year"] = co2e.loc[:, "co2e_tonnes_via_mmbtu"].where(
+        condition, other=alternative
+    )
+    plant_co2e = co2e.groupby("plant_id_eia")["co2e_tonnes_per_year"].sum()
+    return plant_co2e
+
+
+def _get_plant_location_data(pudl_engine: sa.engine.Engine) -> pd.DataFrame:
+    query = """
+    select
+        plant_id_eia,
+        state,
+        county
+    from plants_entity_eia
+    where report_date >= date('2020-01-01') -- this is monthly data
+    AND fuel_type_code_pudl in ('coal', 'gas', 'oil')
+    ;
+    """
+    df = pd.read_sql(query, pudl_engine)
+    return df
+
+
+def _transfrom_plant_location_data(
+    plant_locations: pd.DataFrame, state_table: pd.DataFrame, county_table: pd.DataFrame
+) -> pd.DataFrame:
+    plant_locations = add_county_fips_with_backup_geocoding(
+        plant_locations, state_col="state", locality_col="county"
+    )
+    plant_locations = plant_locations.merge(
+        state_table.loc[:, ["state_id_fips", "state_name"]],
+        on="state_id_fips",
+        how="left",
+        copy=False,
+    )
+    plant_locations = plant_locations.merge(
+        county_table.loc[:, ["county_id_fips", "county_name"]],
+        on="county_id_fips",
+        how="left",
+        copy=False,
+    )
+    plant_locations.drop(
+        columns=[
+            "geocoded_locality_name",
+            "geocoded_locality_type",
+            "geocoded_containing_county",
+            "state",
+            "county",
+        ],
+        inplace=True,
+    )
+    plant_locations.rename(
+        columns={"state_name": "state", "county_name": "county"}, inplace=True
+    )
+    return plant_locations
+
+
+def make_existing_co2_table(
+    postgres_engine: sa.engine.Engine, pudl_engine: sa.engine.Engine
+) -> pd.DataFrame:
+    gen_fuel_923 = _get_existing_plant_fuel_data()
+    plant_co2e = _estimate_existing_co2e(gen_fuel_923)
+    states = _get_state_fips_df(postgres_engine)
+    counties = _get_county_fips_df(postgres_engine)
+    plant_locations = _get_plant_location_data()
+    plant_locations = _transfrom_plant_location_data(
+        plant_locations, state_table=states, county_table=counties
+    )
+
     plant_data["facility_type"] = "existing_power"
     plant_data.rename(
         columns={"plant_id_eia": "id", "fuel_type_code_pudl": "resource"},
         inplace=True,
     )
-    _harmonize_fuels_with_iso_queue(plant_data)
+
     plant_data.drop(
         columns=[
             "total_mmbtu",
