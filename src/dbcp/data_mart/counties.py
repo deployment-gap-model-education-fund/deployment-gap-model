b5b08b025af55904899bcbb4d25209df0e897de2
bendnorman <bdn29@cornell.edu>
2024-02-13 11:50:33 -0900

    Remove existing battery co2 emissions from columns to drop in counties data mart

--- src/dbcp/data_mart/counties.py
+++ src/dbcp/data_mart/counties.py
@@ -587,7 +587,6 @@ def _convert_long_to_wide(long_format: pd.DataFrame) -> pd.DataFrame:
         # A handful of hybrid facilities with co-located diesel generators.
         # They produce tiny amounts of CO2 but large amounts of confusion.
         "onshore_wind_existing_co2e_tonnes_per_year",
-        "battery_storage_existing_co2e_tonnes_per_year",
         "renewable_and_battery_proposed_co2e_tonnes_per_year",  # not currently modeled
         # No superset proposed_facility_counts due to double-counting multi-resource projects.
         # I recalculate those from the project data in _get_category_project_counts()
--- src/dbcp/validation/tests.py
+++ src/dbcp/validation/tests.py
@@ -236,7 +236,7 @@ def test_county_wide_coverage(engine: Engine):
     ), "counties_wide_format does not contain all counties"
     notnull = df.notnull()
     assert (
-        notnull.any(axis=1).sum() == 2375
+        notnull.any(axis=1).sum() == 2388
     ), f"counties_wide_format has unexpected county coverage: {notnull[notnull.any(axis=1)]}"
 
 
