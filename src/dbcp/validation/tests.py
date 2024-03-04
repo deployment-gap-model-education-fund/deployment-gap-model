2aa232f8700868e29c6d056a1123bb756fe6e650
bendnorman <bdn29@cornell.edu>
2024-03-04 14:37:20 -0900

    Adjust ETL to work with oldest GS data. Add simple ISO comparison

--- src/dbcp/validation/tests.py
+++ src/dbcp/validation/tests.py
@@ -236,7 +236,7 @@ def test_county_wide_coverage(engine: Engine):
     ), "counties_wide_format does not contain all counties"
     notnull = df.notnull()
     assert (
-        notnull.any(axis=1).sum() == 2389
+        notnull.any(axis=1).sum() == 2385
     ), f"counties_wide_format has unexpected county coverage: {notnull[notnull.any(axis=1)]}"
 
 
