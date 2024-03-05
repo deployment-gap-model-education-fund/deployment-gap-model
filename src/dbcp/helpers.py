a0bcfdb7677d3dfaf30aa79d5a6fb94a8dd112d4
bendnorman <bdn29@cornell.edu>
2024-03-04 16:49:54 -0900

    Rework pudl caching function

--- src/dbcp/helpers.py
+++ src/dbcp/helpers.py
@@ -7,14 +7,11 @@ import shutil
 from io import StringIO
 from pathlib import Path
 
-import boto3
-import botocore
+import fsspec
 import google.auth
 import pandas as pd
 import pandas_gbq
 import sqlalchemy as sa
-from botocore import UNSIGNED
-from botocore.config import Config
 from google.cloud import bigquery
 from tqdm import tqdm
 
@@ -112,7 +109,7 @@ def get_pudl_engine() -> sa.engine.Engine:
     return pudl_engine
 
 
-def get_pudl_resource(pudl_resource: str) -> Path:
+def get_pudl_resource(pudl_resource: str, bucket: str) -> Path:
     """Given the name of a PUDL resource, return the path to the cached file.
 
     If the file is not cached, download it from S3 and return the path.
@@ -123,65 +120,39 @@ def get_pudl_resource(pudl_resource: str) -> Path:
         pudl_resource_path: The path to the cached PUDL resource.
     """
     PUDL_VERSION = os.environ["PUDL_VERSION"]
-    pudl_output_bucket = "pudl.catalyst.coop"
 
     pudl_cache = Path("/app/data/data_cache/pudl/")
     pudl_cache.mkdir(exist_ok=True)
     pudl_version_cache = pudl_cache / PUDL_VERSION
     pudl_version_cache.mkdir(exist_ok=True)
 
-    s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+    if bucket == "gs://parquet.catalyst.coop":
+        pudl_version_cache = pudl_version_cache / "parquet"
+        pudl_version_cache.mkdir(exist_ok=True)
 
-    pudl_resource_path = pudl_version_cache / pudl_resource
-    # .gz files are unzipped then deleted so we need to check
-    # the unzipped file exists
-    unzipped_pudl_resource_path = Path(
-        str(pudl_resource_path).replace(".sqlite.gz", ".sqlite")
-    )
+    remote_pudl_resource_path = f"{bucket}/{PUDL_VERSION}/{pudl_resource}"
+    local_pudl_resource_path = pudl_version_cache / pudl_resource
 
-    if unzipped_pudl_resource_path.exists():
-        logger.info(
-            f"{pudl_resource} exists in the cache at {unzipped_pudl_resource_path}."
-        )
-    else:
-        logger.info(
-            f"{pudl_resource} does not exist in the cache, downloading from AWS."
-        )
-        # check the pudl_resource exists in s3
-        try:
-            s3.head_object(
-                Bucket=pudl_output_bucket, Key=f"{PUDL_VERSION}/{pudl_resource}"
-            )
-        except botocore.exceptions.ClientError:
-            raise ValueError(
-                f"{PUDL_VERSION}/{pudl_resource} does not exist in the s3://{pudl_output_bucket}."
-            )
+    with fsspec.open(
+        f"filecache::{remote_pudl_resource_path}",
+        s3={"anon": True},
+        filecache={"cache_storage": str(pudl_version_cache / "fsspec")},
+    ) as fo:
+        # Write the content to the local file
+        with open(local_pudl_resource_path, "wb") as local_file:
+            local_file.write(fo.read())
 
-        # download the pudl_resource from s3 with a tqdm progress bar
-        logger.info(f"Downloading {pudl_resource}.")
-        pudl_resource_size = s3.head_object(
-            Bucket=pudl_output_bucket, Key=f"{PUDL_VERSION}/{pudl_resource}"
-        )["ContentLength"]
-        with tqdm(total=100, desc=f"Downloading {pudl_resource}") as pbar:
-
-            def progress_hook(bytes_amount):
-                pbar.update(bytes_amount / pudl_resource_size * 100)
-
-            s3.download_file(
-                pudl_output_bucket,
-                f"{PUDL_VERSION}/{pudl_resource}",
-                str(pudl_resource_path),
-                Callback=progress_hook,
-            )
-
-        # if pudl_resource_path is a gzip file, unzip it
-        logger.info(f"Unzipping {pudl_resource}.")
-        if pudl_resource_path.suffix == ".gz":
-            with gzip.open(pudl_resource_path, "rb") as f_in:
-                with open(pudl_resource_path.with_suffix(""), "wb") as f_out:
+    # if local_pudl_resource_path is a gzip file and isn't already unzipped, unzip it
+    if local_pudl_resource_path.suffix == ".gz":
+        unzipped_pudl_resource_path = local_pudl_resource_path.with_suffix("")
+        if not unzipped_pudl_resource_path.exists():
+            with gzip.open(local_pudl_resource_path, "rb") as f_in:
+                with open(unzipped_pudl_resource_path, "wb") as f_out:
                     shutil.copyfileobj(f_in, f_out)
-            pudl_resource_path.unlink()
-    return unzipped_pudl_resource_path
+        local_pudl_resource_path.unlink()
+        return unzipped_pudl_resource_path
+
+    return local_pudl_resource_path
 
 
 def track_tar_progress(members):
