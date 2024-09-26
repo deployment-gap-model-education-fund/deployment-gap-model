37d20a33ce5fa217c0255fbde4e68d1216c6484c
bendnorman <bdn29@cornell.edu>
2024-09-25 17:28:30 -0800

    Update settings file path

--- src/dbcp/commands/settings.py
+++ src/dbcp/commands/settings.py
@@ -1,4 +1,5 @@
 """Save settings to a YAML file."""
+
 import click
 
 from dbcp.archivers.utils import ExtractionSettings
@@ -7,6 +8,6 @@ from dbcp.archivers.utils import ExtractionSettings
 @click.command()
 def save_settings():
     """Update and save the ETL settings to a YAML file."""
-    es = ExtractionSettings.from_yaml("/app/dbcp/etl_settings.yaml")
+    es = ExtractionSettings.from_yaml("/app/dbcp/settings.yaml")
     es.update_archive_generation_numbers()
     es.to_yaml("/app/dbcp/settings.yaml")
