81e0fadbd9122181bda80df13c3c283be6e84357
Katie Lamb <katherine.lamb@catalyst.coop>
2025-07-11 09:48:20 -0700

    update with june 2025 data

--- src/dbcp/etl.py
+++ src/dbcp/etl.py
@@ -192,7 +192,7 @@ def etl_manual_ordinances() -> dict[str, pd.DataFrame]:
 
 def etl_acp_projects() -> dict[str, pd.DataFrame]:
     """ETL ACP projects."""
-    acp_uri = "gs://dgm-archive/acp/projects_Q1_2025.csv"
+    acp_uri = "gs://dgm-archive/acp/projects_06_24_2025.csv"
     raw_dfs = dbcp.extract.acp_projects.extract(acp_uri)
     transformed = dbcp.transform.acp_projects.transform(raw_dfs)
     return transformed
